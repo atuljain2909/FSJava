9401fcb9a64a52fbbf13c0c545a9060bace46888
Atul Jain <atuljain.2909@gmail.com>
2024-03-26 20:21:34 +0530

    c2_day4_26March2024

--- src/main/java/School.java
+++ src/main/java/School.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class School {
+public class School {
+
+    String schoolName;
+    String principalName;
+    String schoolAddress;
+    int teacherCount;
+    int studentCount;
+    float teacherStudentRatio;
+
+    public void displaySchoolInformation(){
+        System.out.println("School Name is " + schoolName);
+        System.out.println("School's Principal name is " + principalName);
+        System.out.println("School's Address is " + schoolAddress);
+        System.out.println("Teacher Count is " + teacherCount);
+        System.out.println("Student Count is " + studentCount);
+        System.out.println("Teacher Student Ratio is " + teacherStudentRatio);
+    }
+
+    public void displaySchoolResult(){
+        System.out.println("Everyone passed with flying colours.");
+    }
 }
--- src/main/java/Teacher.java
+++ src/main/java/Teacher.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Teacher {
+public class Teacher {
+
+    String teacherName;
+    String teacherGender;
+    String favouriteSubject;
+
+    public void displayTeacherInformation(){
+        System.out.println("Teacher Name is " + teacherName);
+        System.out.println("Teacher's Gender is " + teacherGender);
+        System.out.println("School's favourite subject is " + favouriteSubject);
+    }
 }
