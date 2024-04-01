8ddc4b4d5d68d31017ae58460df05c433eff8022
Atul Jain <atuljain.2909@gmail.com>
2024-04-01 17:59:43 +0530

    day5

---
+++ src/main/java/DataType.java
@@ -0,0 +1,19 @@
+public class DataType {
+
+
+
+    public static void main(String[] args){
+
+        byte b1 = -128;
+        short s1 = 32767;
+
+        int i1 = 2147483647;
+        int i2 = 2147483647;
+        int i3 = i1 + i2;
+
+        long l1 = 1474836470;
+        System.out.println(i3);
+    }
+
+
+}
